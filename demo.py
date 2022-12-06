ed5b14a10049ce3223177c671bca09f35f84921e
FengxinLee <45204195+FengxinLee@users.noreply.github.com>
2022-12-06 15:38:59 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -29,26 +29,24 @@ def set_seed(seed=1):
 def get_parser():
     parser = argparse.ArgumentParser(description='Demo')
     parser.add_argument('--pose_weights', default='./model/best_model_pose.pt', help='the weights for network initialization')
+    parser.add_argument('--pose_file', default='./data/sample.pkl', help='the input pose array of video side')
+    parser.add_argument('--top_k', default=5, type=int, help='recommend top k list')
     return parser
     
 
-def load_model():
-    arg = get_parser().parse_args()
+def load_model(weights):
     posemodel = PoseModel_Disentangle(embedding_size=256, num_cat=4, layer=3, edge_importance_weighting=True)
-    if arg.pose_weights is not None:
-        posemodel.load_state_dict(torch.load(arg.pose_weights))
-    else:
-        raise ValueError("Do Not Exist Pose Model Weights")
+    posemodel.load_state_dict(torch.load(weights))
     posemodel.eval()
     return posemodel
     
-def get_data():
-    video_id = 70
-    video_arrary = pickle.load(open('./data/'+ str(video_id) +'_sample.pkl', 'rb'))
+def get_data(filename):
+    
+    video_arrary = pickle.load(open(filename, 'rb'))
     video_arrary = torch.Tensor(video_arrary)
     video_arrary = video_arrary.unsqueeze(4)
     video_arrary = video_arrary.permute(0, 3, 1, 2, 4).contiguous()
-    video_arrary = video_arrary[5:10]
+    # video_arrary = video_arrary[5:10]
 
     item_embedding = pickle.load(open('./data/item_feature.pkl', 'rb'))
     for i in range(len(item_embedding)):
@@ -63,8 +61,14 @@ def get_data():
 
 if __name__ == "__main__":
     set_seed(1)
-    video_arrary, item_embedding, prototype_dis, item_data = get_data()
-    posemodel = load_model()
+
+    arg = get_parser().parse_args()
+
+    pose_file = arg.pose_file
+    pose_weights = arg.pose_weights
+
+    video_arrary, item_embedding, prototype_dis, item_data = get_data(filename=pose_file)
+    posemodel = load_model(weights=pose_weights)
 
     video_feature = posemodel(video_arrary)
     
@@ -74,7 +78,7 @@ if __name__ == "__main__":
         all_score = torch.matmul(video_feature[i], item_embedding[i].T)
         rank_score += torch.mul(prototype_dis[:,i], all_score)
 
-    k = 5
+    k = arg.top_k
     top_k = torch.topk(rank_score, k=k, dim=1)[1].cpu().numpy()
     for i in range(rank_score.shape[0]):
         for j in range(k):
